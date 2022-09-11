a156ce3e8b5b8ecdfd8871de62f1c2938e086648
deneb-dev <93263095+deneb-dev@users.noreply.github.com>
2022-09-11 21:02:18 +0800

    Update channel.cpp

--- channel.cpp
+++ channel.cpp
@@ -5,19 +5,21 @@ void main()
 {
     int tv;
 
-    cout<<"*********DAFTAR CHANNEL TV***********"<<endl;
-    cout<<"1. RCTI"<<endl;
+    cout<<"*********DAFTAR CHANNEL TV NASIONAL***********"<<endl;
+    cout<<"1. GLOBAL TV"<<endl;
     cout<<"2. ANTV"<<endl;
-    cout<<"3. TRANS TV"<<endl;
-    cout<<"---------------------------------------"<<endl;
+    cout<<"3. TV ONE"<<endl;
+    cout<<"3. SCTV"<<endl;
+    cout<<"----------------------------------------------"<<endl;
 
-    cout<<"Masukan Channel Pilihan = ";
+    cout<<"Masukan Channel Pilihan Anda = ";
     cin>>tv;
 
     switch(tv) {
-    case 1 : cout<<"Chanel yang anda pilih adalah RCTI"; break;
+    case 1 : cout<<"Chanel yang anda pilih adalah GLOBAL TV"; break;
     case 2 : cout<<"Channel yang anda pilih adalah ANTV"; break;
-    case 3 : cout<<"Channel yang anda pilih adalah TRANS TV"; break;
+    case 3 : cout<<"Channel yang anda pilih adalah TV ONE"; break;
+    case 3 : cout<<"Channel yang anda pilih adalah SCTV"; break;
 
     default : cout<<"channel tidak tersedia";
     }
